aec6dcea1983f13995497e33016f2877669caab1
MaximSiebert <maxim.siebert@gmail.com>
2020-06-14 12:43:33 -0400

    initial css

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        green: {
+          '500': '#4A9462'
+        },
+        orange: {
+          '500': '#C34F2E'
+        },
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
