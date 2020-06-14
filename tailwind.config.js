5e81aac334cea0b6c3298c441ececbe9588612e8
MaximSiebert <maxim.siebert@gmail.com>
2020-06-14 12:57:19 -0400

    fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   purge: [],
   theme: {
+    fontFamily: {
+      'sans': ['Open Sans', 'system-ui', '-apple-system', 'BlinkMacSystemFont', "Segoe UI", 'Roboto', "Helvetica Neue", 'Arial', "Noto Sans", 'sans-serif', "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
+      'serif': ['Gelasio', 'Georgia', 'Cambria', "Times New Roman", 'Times', 'serif']
+    },
     extend: {
       colors: {
         green: {
